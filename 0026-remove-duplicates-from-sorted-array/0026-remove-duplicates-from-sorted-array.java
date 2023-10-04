dce0760c1305918c3d16ecdd4889cd99fe709412
Sajaltripathi07 <136965660+Sajaltripathi07@users.noreply.github.com>
2023-10-04 18:19:49 +0530

    Time: 1 ms (96.68%), Space: 43.6 MB (74.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] a) {
-        int i=1;
-        for(int j=1;j<a.length;j++){
-        if(a[j]!=a[j-1]){
-            a[i]=a[j];
-            i++;
-         }
-            else{
-                continue;
-                }
+        int count =1;
+        if(a.length==0){
+            return 0;
+        }
+        else{
+        
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]!=a[i+1]){
+                
+              a[count]=a[i+1];
+                count++;
+            }
         
         }
-    return i;    }
-        
+        }
+        return count;
     }
+}
